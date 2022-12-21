0d6955618e3f4f292087920497a3f3699be67589
[huibinkim] <[abc00127@naver.com]>
2022-12-21 16:05:29 +0900

    css 수정사항 (no dim)

--- js/gnb-ui.js
+++ js/gnb-ui.js
@@ -1,24 +1,24 @@
 // // gnb 메뉴 아이콘 클릭시, show
-// $('.btn-menu02').on('click', function(){
-//   $('.gnb-wrapper').addClass('active');
-//   });
-
-// $('.close-btn').on('click', function(){
-//   $('.gnb-wrapper').removeClass('active');
-// });
-
-// gnb 메뉴 아이콘 클릭시, show & dim 처리 
 $('.btn-menu02').on('click', function(){
-  $('.gnb-wrapper').addClass('active40');
-  $('.gnb-overlay').addClass('active');
+  $('.gnb-wrapper').addClass('active');
   });
 
 $('.close-btn').on('click', function(){
-  $('.gnb-wrapper').removeClass('active40');
-  $('.gnb-overlay').removeClass('active');
+  $('.gnb-wrapper').removeClass('active');
 });
 
-$('.gnb-overlay').on('click', function(){
-  $('.gnb-wrapper').removeClass('active40');
-  $('.gnb-overlay').removeClass('active');
-})
+// gnb 메뉴 아이콘 클릭시, show & dim 처리 
+// $('.btn-menu02').on('click', function(){
+//   $('.gnb-wrapper').addClass('active40');
+//   $('.gnb-overlay').addClass('active');
+//   });
+
+// $('.close-btn').on('click', function(){
+//   $('.gnb-wrapper').removeClass('active40');
+//   $('.gnb-overlay').removeClass('active');
+// });
+
+// $('.gnb-overlay').on('click', function(){
+//   $('.gnb-wrapper').removeClass('active40');
+//   $('.gnb-overlay').removeClass('active');
+// })
