c7684ce43fdbe52cd6c08abd20ca3ea598cb72ad
[huibinkim] <[abc00127@naver.com]>
2022-12-21 14:48:06 +0900

    title, overlay.active 추가 및 수정, 링크 추가

--- js/gnb-ui.js
+++ js/gnb-ui.js
@@ -10,15 +10,15 @@
 // gnb 메뉴 아이콘 클릭시, show & dim 처리 
 $('.btn-menu02').on('click', function(){
   $('.gnb-wrapper').addClass('active40');
-  $('.gnb-overlay').removeClass('blind');
+  $('.gnb-overlay').addClass('active');
   });
 
 $('.close-btn').on('click', function(){
   $('.gnb-wrapper').removeClass('active40');
-  $('.gnb-overlay').addClass('blind');
+  $('.gnb-overlay').removeClass('active');
 });
 
 $('.gnb-overlay').on('click', function(){
   $('.gnb-wrapper').removeClass('active40');
-  $('.gnb-overlay').addClass('blind');
+  $('.gnb-overlay').removeClass('active');
 })
