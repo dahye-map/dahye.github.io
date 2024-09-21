ba8be3daec3fe51bc50872f6a3a3b77d399af2d0
dahye <dahae5919@gmail.com>
2024-09-22 00:34:17 +0900

    2024 포트폴리오 리뉴얼

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,30 @@
-/*
-	Epilogue by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+var COMPONENT_UI = (function (cp, $) {
+  $(document).ready(function() {
+    AOS.init({
+      duration: '1000',
+      once: 'true',
+      mirror: 'true'
+    });
+  });
+  
+  cp.scroll = {
+    header: function() {
+      $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
 
-(function($) {
+        if(scrollTop > 100) {
+          $('header').addClass('active')
+        } else {
+          $('header').removeClass('active')
+        }
+        
+      });
+    },
+  }
+  cp.init = function () {
+    cp.scroll.header();
+  };
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
-
-	$(function() {
-
-		var	$window = $(window),
-			$body = $('body');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Items.
-			$('.item').each(function() {
-
-				var $this = $(this),
-					$header = $this.find('header'),
-					$a = $header.find('a'),
-					$img = $header.find('img');
-
-				// Set background.
-					$a.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Remove original image.
-					$img.remove();
-
-			});
-
-			var _tab = $('.tab').offset().top;
-			$(window).scroll(function(){
-				var _scrollTop = $(window).scrollTop();
-
-				if( _scrollTop >= _tab) {
-					$('.tab').addClass('active');
-				} else {
-					$('.tab').removeClass('active');
-				}
-			});
-
-	});
-
-})(jQuery);
+  cp.init();
+  return cp;
+}(window.COMPONENT_UI || {}, jQuery));
